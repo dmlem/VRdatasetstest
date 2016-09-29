9b0c38cb648ffa915b0251d6c7891d30fdf60415
Diana Lem <lemd@email.sc.edu>
2016-09-29 07:26:44 -0400

    Generator Script
    The Generator Script to cause an object to generate instances of itself
    in random positions

---
+++ Clusters/Assets/Generator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+
+    public Transform point;
+    public int numToSpawn;
+    public Vector3 position;
+
+    // Use this for initialization
+    void Start () {
+        int spawned = 0;
+
+        while (spawned < numToSpawn)
+        {
+            position = new Vector3(Random.Range(100.0F, 1000.0F), 70, Random.Range(100.0F, 1000.0F));
+            Instantiate(point, position, Quaternion.identity);
+            spawned++;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
